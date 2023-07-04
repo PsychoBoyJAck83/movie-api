fb2c51035889a74c20548aebbe1699132c9be5d6
PsychoBoyJAck83 <georgitsis.theodoros@gmail.com>
2023-07-04 23:08:17 +0200

    required auth.js file

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let auth = require('./auth')(app);
+
 app.use(express.static("public"));
 
 app.use(morgan('combined', {stream: accessLogStream}));
